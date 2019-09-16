2ba69d5407fe83d8e3d1f4078f0fda516986c9bd
ChenpinonHuang <ChenpinonHuang@users.noreply.github.com>
2019-09-16 19:09:05 -0400

    Add project files.

---
+++ Lab1/Controllers/AdditionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class AdditionController : Controller
+    {
+
+        public IActionResult Addition(int x, int y)
+        {
+            
+            return Ok("Result = " + (x + y).ToString());
+
+        }
+
+       
+    }
+}
